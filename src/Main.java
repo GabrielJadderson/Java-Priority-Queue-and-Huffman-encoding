ff62e901d0879f32ab611e48d0e0387624ee9dbf
Gabriel Howard Jadderson <mullie34@hotmail.com>
2017-03-01 13:43:02 +0100

    Project

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by gabriel on 01/03/2017.
  */
@@ -5,7 +7,24 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println(":D");
-        System.out.println(":D");
+        PQ pq = new PQHeap(1000);
+
+        int n = 0;
+        int i;
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt())
+
+        {
+            i = sc.nextInt();
+            n++;
+            pq.insert(new Element(i, null));
+        }
+        while (n > 0)
+
+        {
+            System.out.println(pq.extractMin().key);
+            n--;
+        }
     }
+
 }
---
+++ src/PQHeap.java
@@ -0,0 +1,21 @@
+/**
+ * Created by gabriel on 01/03/2017.
+ */
+public class PQHeap implements PQ
+{
+    public PQHeap(int n) {
+
+    }
+
+    @Override
+    public Element extractMin()
+    {
+        return null;
+    }
+
+    @Override
+    public void insert(Element e)
+    {
+
+    }
+}
