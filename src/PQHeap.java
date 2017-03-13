3d708b80de814f1a4a9f786dc09d959723c0de86
Gabriel Howard Jadderson <mullie34@hotmail.com>
2017-03-13 11:27:35 +0100

    Final commit
    Final commit

--- src/PQHeap.java
+++ src/PQHeap.java
@@ -1,11 +1,17 @@
 /**
- * Created by Gabriel(gajad16) Patrick(pajak16) on 01/03/2017.
+ * Created by Gabriel Jadderson(gajad16) Patrick Jakobsen(pajak16) on 01/03/2017.
+ * @author gajad16, pajak16
  */
 public class PQHeap implements PQ
 {
     protected Element[] data;
     protected int dataCount;
 
+    /**
+     * Creates a PQHeap with a given number of {@link Element} to be stored
+     *
+     * @param n number of elements in the heap
+     */
     public PQHeap(int n)
     {
         //The first element is unused to make the arithmetic easier and faster.
@@ -14,6 +20,13 @@ public class PQHeap implements PQ
         dataCount = 0;
     }
 
+    /**
+     * Extracts the first element stored in the heap.
+     * Doing so will also remove the element from the heap.
+     * <p>This method utilitzes {@link #MinHeapify(int) MinHeapify} to maintain heap-order once an element has been deleted</p>
+     *
+     * @return The first {@link Element} ie. The Element in the heap which contains the minimum key value.
+     */
     @Override
     public Element extractMin()
     {
@@ -34,6 +47,11 @@ public class PQHeap implements PQ
         return e;
     }
 
+    /**
+     * Inserts an {@link Element} to the heap as long as there's room for the Element in the Heap.
+     *
+     * @param e {@link Element} to add.
+     */
     @Override
     public void insert(Element e)
     {
@@ -54,12 +72,13 @@ public class PQHeap implements PQ
             }
         }
     }
-  //directX 12 best library the best so good, make gaming great again.
 
     /**
-     * Constructs min heap order for the given node index and its children. If a modification is made, then it min heap orders the subtree in the modified direction.
+     * Constructs min heap order for the given node index and its children.
+     * If a modification is made, then it min heap orders the subtree in the modified direction.
      *
      * @param i Node index to heap order.
+     * @see PQHeap#exchange(int, int)
      */
     private void MinHeapify(int i)
     {
@@ -86,36 +105,63 @@ public class PQHeap implements PQ
         }
     }
 
+    /**
+     * Swaps two elements in the {@link #data} array
+     *
+     * @param a Index of the element to swap
+     * @param b Index of the element to swap
+     */
     void exchange(int a, int b)
     {
-        //Swap the elements.
+        //Swap the elements. (can be done more efficiently)
         Element da = data[a];
         Element db = data[b];
         data[a] = db;
         data[b] = da;
     }
 
+    /**
+     * Takes in an Integer <b><i>i</i></b> and determines the parent {@link Element} to the given index <b><i>i</i></b>.
+     * Performs a bit-shift to the right, which is equivalent to integer division with 2.
+     *
+     * @param i index
+     * @return <b>\u230Ai/2\u230B</b> or the parent Element to the given index <b><i>i</i></b>.
+     */
     int parent(int i)
     {
-        //This bitshift is equivalent to making an integer division with 2.
+        //This bit-shift is equivalent to making an integer division with 2.
         //All the bits have a value, and by moving them right that value is halved for each, halving the total value.
         //A 1 bit in the far right will be deleted.
+        //Integer division rounds down, hence no floor function is needed.
         return i >> 0x01;
     }
 
+    /**
+     * Takes in an Integer <b><i>i</i></b> and determines the left {@link Element} to the given index <b><i>i</i></b>.
+     * Performs a bit-shift to the left, which is equivalent to multiplying with 2.
+     *
+     * @param i index
+     * @return <b>2*i</b> or the Element to the left of the given index <b><i>i</i></b>.
+     */
     int left(int i)
     {
-        //This bitshift is equivalent to multiplying with 2.
+        //This bit-shift is equivalent to multiplying with 2.
         //All the bits have a value, and by moving them left that value is doubled for each, doubling the total value.
         //If the leftmost bit is 1, then it overflows and something will probably break. A heap of that size is not expected.
         return i << 0x01;
     }
 
+    /**
+     * Takes in an Integer <b><i>i</i></b> and determines the right {@link Element} to the given index <b><i>i</i></b>.
+     * Performs a bit-shift to the left and performing xor on 1, which is equivalent to multiplying with 2 and adding 1.
+     *
+     * @param i index
+     * @return <b>2*i+1</b> or the Element to the right of the given index <b><i>i</i></b>.
+     */
     int right(int i)
     {
         //Same logic as in left(), except that the rightmost bit is set to 1, effectively adding 1 to the total value.
         return i << 0x01 ^ 0x01;
     }
-
 
 }
