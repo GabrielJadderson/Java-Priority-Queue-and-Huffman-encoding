e1e92436d34e45913f87bbe2ba151440b0ca6582
Gabriel Howard Jadderson <mullie34@hotmail.com>
2017-03-23 12:16:57 +0100

    Del2
    Del2

---
+++ src/Dict.java
@@ -0,0 +1,12 @@
+/**
+ * Created by gabriel on 23/03/2017.
+ */
+public interface Dict
+{
+    public void insert(int k);
+
+    public int[] orderedTraversal();
+
+    public boolean search(int k);
+
+}
