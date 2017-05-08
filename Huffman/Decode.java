39815d094141c6d131fb245947b1e80f67d6aeef
Gabriel Howard Jadderson <mullie34@hotmail.com>
2017-05-08 17:44:02 +0200

    A fix that actually works.

--- Huffman/Decode.java
+++ Huffman/Decode.java
@@ -1,67 +1,57 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.stream.IntStream;
 
 /**
  * Created by Gabriel Jadderson on 08/05/2017.
  */
 public class Decode extends Encode
 {
-    //static int sum;
-
     public Decode()
     {
         super();
-        //sum = 0;
     }
 
     public static void main(String[] args)
     {
-        inputFile = args[0];
-        outputFile = args[1];
 
         new Decode();
 
         try
         {
-            FileOutputStream fileOutputStream = new FileOutputStream(new File(outputFile));
-            FileInputStream fileInputStream = new FileInputStream(new File(inputFile));
+            FileInputStream fileInputStream = new FileInputStream(new File(args[0]));
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(args[1]));
 
             BitInputStream bitInputStream = new BitInputStream(fileInputStream);
 
             for (int i = 0; i < frequency.length; i++)
             {
-                //int freq = bitInputStream.readInt();
-                pqHeap.insert(new Element(bitInputStream.readInt(), new Tree(null, null, i)));
-                //sum += freq;
+                int freq = bitInputStream.readInt();
+                pqHeap.insert(new Element(freq, new Tree(null, null, i)));
+                frequency[i] = freq;
             }
+
+            int sum = IntStream.of(frequency).sum();
 
             Tree huffTree = Huffman();
 
-            int charactersWritten = 0;
-            while (true)
+            int c = 0;
+            while (c < sum)
             {
 
                 int val = huffTree.getCharacter(bitInputStream);
                 if (val == -1) break;
-                charactersWritten++;
 
-                System.out.println(val);
                 fileOutputStream.write(val);
-
+                c++;
             }
-
 
             fileInputStream.close();
             bitInputStream.close();
         } catch (Exception e)
         {
             e.printStackTrace();
-        }
-
-        for (int i = 0; i < frequency.length; i++)
-        {
-            //System.out.println(frequency[i]);
         }
 
     }
